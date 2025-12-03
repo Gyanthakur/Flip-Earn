09393cdcda035594420659c55dae9b7a65f56571
Gyanthakur <gps.96169@gmail.com>
2025-12-04 00:11:41 +0530

    feat: implement routing and add initial page components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import MarketPlace from './pages/MarketPlace'
+import MyListing from './pages/MyListing'
+import ListingDetails from './pages/ListingDetails'
+import ManageListing from './pages/ManageListing'
+import Messages from './pages/Messages'
+import MyOrders from './pages/MyOrders'
+import Loading from './pages/Loading'
 
 const App = () => {
   return (
     <div>
       
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/marketplace" element={<MarketPlace />} />
+        <Route path="/my-listing" element={<MyListing />} />
+        <Route path="/listing/:listingId" element={<ListingDetails />} />
+        <Route path="/create-listing" element={<ManageListing />} />
+        <Route path="/edit-listing/:id" element={<ManageListing />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
     </div>
   )
 }
