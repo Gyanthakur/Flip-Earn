4e77e649445003336416787eec07ed7db8875c8b
Gyanthakur <gps.96169@gmail.com>
2025-12-05 19:48:39 +0530

    feat: update page titles, add LatestListing component, and implement listing slice for state management

---
+++ client/src/components/LatestListing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const LatestListing = () => {
+  return (
+    <div className='mt-20 mb-8'>
+      <Title title="Latest Listing" description="Discover the hottest social profiles available right now." />
+    </div>
+  )
+}
+
+export default LatestListing
