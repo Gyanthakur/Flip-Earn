2015e47307ccdb00b3385725e612b367619137d0
Gyanthakur <gps.96169@gmail.com>
2025-12-16 12:06:37 +0530

    feat: implement listing feature with LatestListing and ListingCard components, add Plans and CTA components, and configure Redux store

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestListing from '../components/LatestListing'
+import Plans from '../components/Plans'
+import CTA from '../components/CTA'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestListing/>
+      <Plans/>
+      <CTA/>
     </div>
   )
 }
