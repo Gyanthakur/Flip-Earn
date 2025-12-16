7a1a83769175e6a13954fa597037a54ad643da47
Gyanthakur <gps.96169@gmail.com>
2025-12-16 12:15:16 +0530

    feat: add Footer component to enhance page layout and user experience

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import LatestListing from '../components/LatestListing'
 import Plans from '../components/Plans'
 import CTA from '../components/CTA'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <LatestListing/>
       <Plans/>
       <CTA/>
+      <Footer/>
     </div>
   )
 }
