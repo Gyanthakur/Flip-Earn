52aae10c0e65e11f3e409995a9989b54dd1a88f7
Gyanthakur <gps.96169@gmail.com>
2025-12-16 15:00:14 +0530

    feat: enhance filtering functionality in MarketPlace component with search capability

--- client/src/pages/MarketPlace.jsx
+++ client/src/pages/MarketPlace.jsx
@@ -1,11 +1,14 @@
 import { ArrowLeftIcon, FilterIcon, Verified } from 'lucide-react'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import ListingCard from '../components/ListingCard';
 import FilterSidebar from '../components/FilterSidebar';
 
 const MarketPlace = () => {
+
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
   const navigate = useNavigate();
   const [showFilterPhone, setShowFilterPhone] = useState(false); 
   const [filters, setFilters] = useState({
@@ -19,9 +22,67 @@ const MarketPlace = () => {
   })
 
   const {listings} = useSelector(state => state.listing);
-  const FilteredListings = listings.filter(listing =>{
-    return true;
-  });
+  
+  // const filteredListings = listings.filter((listing) =>{
+
+  //   if(filters.plateform && filters.plateform.length > 0){
+  //     if(!filters.plateform.includes(listing.platform)) return false;
+  //   }
+    
+  //   if(filters.maxPrice){
+  //     if(listing.price > filters.maxPrice) return false;
+  //   }
+
+  //   if(filters.minFollowers){
+  //     if(listing.followers_count < filters.minFollowers) return false;
+  //   }
+
+  //   if(filters.niche && filters.niche.length > 0){
+  //     if(!filters.niche.includes(listing.niche)) return false;
+  //   }
+
+  //   if(filters.Verified && listing.verified !== filters.Verified) return false;
+  //   if(filters.monetized && listing.monetized !== filters.monetized) return false;
+    
+  //   return true;
+  // });
+
+  const filteredListings = listings.filter((listing) => {
+
+  if (filters.platform && filters.platform.length > 0) {
+    if (!filters.platform.includes(listing.platform)) return false;
+  }
+
+  if (filters.maxPrice) {
+    if (listing.price > filters.maxPrice) return false;
+  }
+
+  if (filters.minFollowers) {
+    if (listing.followers_count < filters.minFollowers) return false;
+  }
+
+  if (filters.niche && filters.niche.length > 0) {
+    if (!filters.niche.includes(listing.niche)) return false;
+  }
+
+  if (filters.Verified && listing.verified !== filters.Verified) return false;
+  if (filters.monetized && listing.monetized !== filters.monetized) return false;
+
+  if(search){
+    const trimed = search.trim();
+    if(
+      !listing.title.toLowerCase().includes(trimed.toLowerCase()) &&
+      !listing.username.toLowerCase().includes(trimed.toLowerCase()) &&
+      !listing.description.toLowerCase().includes(trimed.toLowerCase()) &&
+      !listing.platform.toLowerCase().includes(trimed.toLowerCase()) &&
+      !listing.niche.toLowerCase().includes(trimed.toLowerCase())
+    ) return false;
+  }
+
+  return true;
+});
+
+
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
 
@@ -40,7 +101,7 @@ const MarketPlace = () => {
       <div className='relative flex items-start justify-between gap-8 pb-8'>
         <FilterSidebar setFilters={setFilters} filters={filters} setShowFilterPhone={setShowFilterPhone} showFilterPhone={showFilterPhone}/>
         <div className='flex-1 grid xl:grid-cols-2 gap-4'>
-          {FilteredListings.sort((a,b) => a.featured ? -1 : b.featured ? 1 : 0).map((listing, index)=>(
+          {filteredListings.sort((a,b) => a.featured ? -1 : b.featured ? 1 : 0).map((listing, index)=>(
             <ListingCard listing={listing} key={index}/>
           ))}
         </div>
