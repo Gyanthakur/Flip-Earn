88b6d578874b4a7607ea3613bcae666920b0f473
Gyanthakur <gps.96169@gmail.com>
2025-12-24 00:33:41 +0530

    feat: refactor ManageListing component with form handling and UI enhancements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const {pathname} = useLocation();
   return (
     <div>
-      <toaster />
+      <Toaster />
       <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>  
       {!pathname.includes('/admin') && !pathname.includes('/projects') && <Navbar />}
      
