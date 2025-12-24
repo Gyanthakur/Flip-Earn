34711460a714ac3900f4f5432655c0012e159925
Gyanthakur <gps.96169@gmail.com>
2025-12-25 00:32:13 +0530

    feat: Add controllers for fetching public and user listings, and updating listings

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -55,3 +55,123 @@ export const addListing = async(req,res) => {
         return res.status(500).json({message: error.code || error.message });
     }
 }
+
+
+// controller for Getting all public listings 
+export const getAllPublickListing = async(req,res) => {
+    try {
+        const listings = await prisma.listing.findMany({
+            where:{status: "active"},
+            include:{owner: true},
+            orderBy: {createdAt: "desc"},
+        })
+
+        if(!listings || listings.length === 0){
+            return res.json({listings: []});
+        }
+
+        return res.json({listings});
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: error.code || error.message });
+    }
+}
+
+// controller for Getting all user Listings
+
+export const getAllUserListing = async(req,res) => {
+    try {
+        const {userId} = await req.auth();
+        // get all listing except deleted
+        const listings = await prisma.listing.findMany({
+            where:{ownerId: userId, status: {not: "deleted"}},
+            orderBy: {createdAt: "desc"},
+        })
+
+        const user = await prisma.user.findUnique({
+            where: {id: userId},
+        })
+
+        const balance = {
+            earned: user.earned,
+            withdrawn: user.withdrawn,
+            available: user.earned - user.withdrawn,
+        }
+
+        if(!listings || listings.length === 0){
+            return res.json({listings: [], balance});
+        }
+        return res.json({listings, balance});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: error.code || error.message });
+    }
+}
+
+
+// controller for updating Listing in database
+export const updateListing = async(req,res)=>{
+    try {
+        const {userId} = await req.auth();
+        const accountDetails = JSON.parse(req.body.accountDetails);
+
+        if(req.files.length + accountDetails.existingImages.length > 5){
+            return res.status(400).json({message: "You can upload maximum 5 images"});
+        }
+
+
+
+        accountDetails.followers_count = parseInt(accountDetails.followers_count);
+        accountDetails.engagement_rate = parseFloat(accountDetails.engagement_rate);
+        accountDetails.monthely_views = parseFloat(accountDetails.monthely_views);
+        accountDetails.price = parseFloat(accountDetails.price);
+        accountDetails.platform = accountDetails.platform.toLowerCase();
+        accountDetails.niche = accountDetails.niche.toLowerCase();
+
+        accountDetails.username.startsWith("@") ? accountDetails.username = accountDetails.username.slice(1) : null;
+        
+        const listing = await prisma.listing.update({
+            where:{id: accountDetails.id, ownerId: userId},
+            data: accountDetails
+        })
+
+        if(!listing){
+            return res.status(404).json({message: "Listing not found"});
+        }
+
+        if(listing.status === "sold"){
+            return res.status(400).json({message: "Sold listing cannot be updated"});
+        }
+
+        if(req.files.length > 0){
+             const uploadImages = req.files.map(async (file)=>{
+            const response = await imagekit.files.upload({
+                file: fs.createReadStream(file.path),
+                fileName: `${Date.now()}-${file.originalname}.png`,
+                folder: "flip-earn",
+                transformation: {pre: "w-1280, h-auto"}
+            });
+            return response.url;
+        })
+
+        // wait for all images to be uploaded
+        const images = await Promise.all(uploadImages);
+
+        const listing = await prisma.listing.update({
+            where:{id: accountDetails.id, ownerId: userId},
+            data: {
+                ownerId: userId,
+                ...accountDetails,
+                images: [...accountDetails.existingImages, ...images]
+            }
+        })
+        return res.status(200).json({message: "Account updated successfully", listing});
+        }
+        return res.status(200).json({message: "Account updated successfully", listing});
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: error.code || error.message });
+    }
+}
