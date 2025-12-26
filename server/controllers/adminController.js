fe95d7405242ad0f69665a79f1e31751399f6bd4
Gyanthakur <gps.96169@gmail.com>
2025-12-26 20:01:16 +0530

    feat: Implement admin controller with various functionalities including dashboard data, listing management, and withdrawal requests

---
+++ server/controllers/adminController.js
@@ -0,0 +1,287 @@
+// controller for checking if user is admin
+
+import prisma from "../configs/prisma.js";
+
+export const isAdmin = async (req, res) => {
+	try {
+		return res.json({ isAdmin: true });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// Controller for getting dashboard data
+export const getDashboard = async (req, res) => {
+	try {
+		const totalListing = await prisma.listing.count({});
+		const transaction = await prisma.transaction.findMany({
+			where: { isPaid: true },
+			select: { amount: true },
+		});
+
+		const totalRevenue = transaction.reduce(
+			(total, transaction) => total + transaction.amount,
+			0
+		);
+
+		const activeListing = prisma.listing.count({
+			where: { status: "active" },
+		});
+
+		const totalUser = await prisma.user.count({});
+
+		const recentListing = await prisma.listing.findMany({
+			orderBy: { createdAt: "desc" },
+			take: 5,
+			include: { owner: true },
+		});
+
+		return res.json({
+			dashboardData: {
+				totalListing,
+				totalRevenue,
+				activeListing,
+				totalUser,
+				recentListing,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// Controller for getting all listing
+export const getAllListing = async (req, res) => {
+	try {
+		const listings = await prisma.listing.findMany({
+			include: { owner: true },
+			orderBy: { createdAt: "desc" },
+		});
+
+		if (!listings || listings.length === 0) {
+			return res.json({ listings: [] });
+		}
+
+		return res.json({ listings });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// Change listing status
+
+export const changeStatus = async (req, res) => {
+	try {
+		const { listingId } = req.params;
+		const { status } = req.body;
+
+		const listing = await prisma.listing.findUnique({
+			where: { id: listingId },
+		});
+
+		if (!listing) {
+			return res.status(404).json({ message: "Listing not found" });
+		}
+
+		await prisma.listing.update({
+			where: { id: listingId },
+			data: { status },
+		});
+
+		return res.json({ message: "Listing status updated" });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// controller for getting all unverified listings with credentials submitted
+export const getAllUnverifiedListings = async (req, res) => {
+	try {
+		const listings = await prisma.listing.findMany({
+			where: {
+				isCredentialSubmitted: true,
+				isCredentialVerified: false,
+				status: { not: "deleted" },
+			},
+			orderBy: { createdAt: "desc" },
+		});
+
+		if (!listings || listings.length === 0) {
+			return res.json({ listings: [] });
+		}
+
+		return res.json({ listings });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// controller for getting credential
+export const getCredential = async (req, res) => {
+	try {
+		const { listingId } = req.params;
+
+		const credential = await prisma.credential.findFirst({
+			where: { listingId },
+		});
+
+		if (!credential) {
+			return res.status(404).json({ message: "Credential not found" });
+		}
+
+		return res.json({ credential });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// Mark credential is verified
+
+export const markCredentialVerified = async (req, res) => {
+	try {
+		const { listingId } = req.params;
+
+		await prisma.listing.update({
+			where: { id: listingId },
+			data: { isCredentialVerified: true },
+		});
+
+		return res.json({ message: "Credential marked as verified" });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// get all un-changed credentials
+export const getAllUnChangedListings = async (req, res) => {
+	try {
+		const listings = await prisma.listing.findMany({
+			where: {
+				isCredentialVerified: true,
+				isCredentialChanged: false,
+				status: { not: "deleted" },
+			},
+			orderBy: { createdAt: "desc" },
+		});
+		if (!listings || listings.length === 0) {
+			return res.json({ listings: [] });
+		}
+
+		return res.json({ listings });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// change credential for verified credentials
+export const changeCredential = async (req, res) => {
+	try {
+		const { listingId } = req.params;
+
+		const { newCredential, credentialId } = req.body;
+
+		await prisma.credential.update({
+			where: { id: listingId },
+			data: { updatedCredential: newCredential },
+		});
+
+		await prisma.listing.update({
+			where: { id: listingId },
+			data: { isCredentialChanged: true },
+		});
+
+		return res.json({ message: "Credential changed successfully" });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// get All transaction
+
+export const getAllTransacrtion = async (req, res) => {
+	try {
+		const transactions = await prisma.transaction.findMany({
+			where: { isPaid: true },
+			orderBy: { createdAt: "desc" },
+			include: { listing: { include: { owner: true } } },
+		});
+
+		//    get customer details for each transaction and add it to the transaction object
+
+		const customers = await prisma.user.findMany({
+			where: { id: { in: transaction.map((t) => t.userId) } },
+			select: { id: true, email: true, name: true, image: true },
+		});
+
+		transactions.forEach((t) => {
+			const customer = customers.find((c) => c.id === t.userId);
+			t.listing.customer = { ...customer };
+		});
+
+		if (!transactions || transactions.length === 0) {
+			return res.json({ transactions: [] });
+		}
+
+		return res.json({ transactions });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// controller for getting all withdraw request
+export const getAllWithdrawRequest = async (req, res) => {
+	try {
+		const requests = await prisma.withdrawal.findMany({
+			orderBy: { createdAt: "asc" },
+			include: { user: true },
+		});
+
+		if (!requests || requests.length === 0) {
+			return res.json({ requests: [] });
+		}
+
+		return res.json({ requests });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
+
+// controller for marking withdrawal is paid
+export const markWithdrawlAsPaid = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const withdrawal = await prisma.withdrawal.findUnique({
+			where: { id },
+		});
+		if (!withdrawal) {
+			return res.status(404).json({ message: "Withdrawal not found" });
+		}
+
+		if (withdrawal.isWithdrawn) {
+			return res
+				.status(400)
+				.json({ message: "Withdrawal already marked as paid" });
+		}
+
+		await prisma.withdrawal.update({
+			where: { id },
+			data: { isWithdrawn: true },
+		});
+
+		return res.json({ message: "Withdrawal marked as paid" });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: error.code || error.message });
+	}
+};
--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,5 +1,7 @@
+import {clerkClient} from "@clerk/express"
 
-export const protect = async (req,res, next) => {
+
+export const protect = async (req, res, next) => {
     try {
         const {userId, has} = await req.auth();
 
@@ -10,6 +12,23 @@ export const protect = async (req,res, next) => {
         const hasPremiupPlan = await has({plan: "premium"});
         req.plan = hasPremiupPlan ? "premium" : "free";
         return next();
+
+    } catch (error) {
+        console.error("Auth Middleware Error:", error);
+        return res.status(401).json({message: error.code || error.message });
+    }
+}
+
+export const protectAdmin = async (req, res, next) => {
+    try {
+        const {user} = await clerkClient.users.getUser(await req.auth().userId)
+
+       const isAdmin = process.env.ADMIN_EMAIL.split(",").includes(user.emailAddresses[0].emailAddress);
+       if(!isAdmin){
+        return res.status(401).json({message: "Unauthorized"})
+       }
+
+       return next();
 
     } catch (error) {
         console.error("Auth Middleware Error:", error);
