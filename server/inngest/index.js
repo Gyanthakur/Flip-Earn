e764f04838bf41f5be8665fc63cfc69dbcce98a1
Gyanthakur <gps.96169@gmail.com>
2026-01-17 19:12:12 +0530

    feat: send mail to admin when user added new listing or account

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -479,6 +479,7 @@ const sendListingCreatedEmail = inngest.createFunction(
     });
 
     /* ================= ADMIN EMAIL ================= */
+    const LISTING_URL = `https://flip-earn-gps.vercel.app/listing?listingId=${listingId}`;
     await sendEmail({
       to: ADMIN_EMAIL,
       subject: "🚨 New Listing Created on FlipEarn",
@@ -512,6 +513,21 @@ const sendListingCreatedEmail = inngest.createFunction(
                       <p><strong>Username:</strong> @${listing.username}</p>
                       <p><strong>Platform:</strong> ${listing.platform}</p>
                     </div>
+
+                    <p style="font-size:14px;color:#4b5563;margin-bottom:25px;">
+                   New Listing added, Please review and verify this listing from the admin dashboard.
+                  </p>
+
+
+                     <!-- Verify Button -->
+                  <div style="text-align:center;">
+                    <a href="${LISTING_URL}"
+                      style="display:inline-block;padding:14px 28px;
+                      background:#4f46e5;color:#ffffff;text-decoration:none;
+                      border-radius:8px;font-size:14px;font-weight:600;">
+                      See Listing
+                    </a>
+                  </div>
 
                     <p style="font-size:14px;color:#4b5563;">
                       Please review and verify this listing from the admin dashboard.
